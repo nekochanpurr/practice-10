2e349118adb6f9d4c309cb5bc8497fbba279529a
Stacy <103967405+nekochanpurr@users.noreply.github.com>
2022-04-25 19:38:47 -0400

    Update practice 10.cpp

--- practice 10.cpp
+++ practice 10.cpp
@@ -1,4 +1,5 @@
 
+
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -26,17 +27,17 @@ int main()
 		"I will give you the information back.";
 	
 		cout << "\nPlease enter the name of five family members.\n ";
-		for (int i = 0; i <= 5; i++)
-			cin.getline(cin, name);
+		for (int i = 0; i <= 4; i++)
+			getline(cin,name[0]);
 	
 
 			cout << "\nPlease enter the birth city of five family members. \n";
-			for (int i = 0; i <= 5; i++)
-			cin.getline (cin, birthCity);
+			for (int i = 0; i <= 4; i++)
+			getline(cin, birthCity[0]);
 			
 			cout << "\nPlease enter the birth year of five family members between 1900-2018.\n ";
-			for (int i = 0; i <= 5; i++)
-			cin.getline(cin,birthYr);
+			for (int i = 0; i <= 4; i++)
+				cin >> birthYr[0];
 
 //feeding information back out to the user
 
