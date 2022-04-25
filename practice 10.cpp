2c9acfbcf3e528dbc129adfd5b5ca63b3b83b8a0
Stacy <103967405+nekochanpurr@users.noreply.github.com>
2022-04-25 19:55:41 -0400

    Update practice 10.cpp

--- practice 10.cpp
+++ practice 10.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 #include <iomanip>
 #include <string>
@@ -13,7 +12,7 @@ const int CAP = 5;
  string name[CAP];
  string birthCity[CAP];
 double birthYr[CAP];
-int sub;
+
 int allInfo = 1;
 
 
@@ -28,24 +27,24 @@ int main()
 	
 		cout << "\nPlease enter the name of five family members.\n ";
 		for (int i = 0; i <= 4; i++)
-			getline(cin,name[0]);
+			getline(cin,name[0,1,2,3,4]);
 	
 
 			cout << "\nPlease enter the birth city of five family members. \n";
 			for (int i = 0; i <= 4; i++)
-			getline(cin, birthCity[0]);
+			getline(cin, birthCity[0,1,2,3,4]);
 			
 			cout << "\nPlease enter the birth year of five family members between 1900-2018.\n ";
 			for (int i = 0; i <= 4; i++)
-				cin >> birthYr[0];
+				cin >> birthYr[0,1,2,3,4];
 
 //feeding information back out to the user
 
-				for (int sub = 0; sub <= allInfo; sub++)
+				for (int i = 0; i <= allInfo; i++)
 				{
-					cout << "Name: " << name[sub] << endl;
-					cout << "Birth City: " << birthCity[sub] << endl;
-					cout << "Birth Year: " << birthYr[sub] << endl;
+					cout << "Name: " << name[0,1,2,3,4] << endl;
+					cout << "Birth City: " << birthCity[0,1,2,3,4] << endl;
+					cout << "Birth Year: " << birthYr[0,1,2,3,4] << endl;
                  }
 
 
