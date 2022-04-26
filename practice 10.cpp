3bd60b0458f70ee42fefa038df7114107be08e1f
Stacy <103967405+nekochanpurr@users.noreply.github.com>
2022-04-25 20:51:16 -0400

    Update practice 10.cpp

--- practice 10.cpp
+++ practice 10.cpp
@@ -13,7 +13,7 @@ const int CAP = 5;
  string birthCity[CAP];
 double birthYr[CAP];
 
-int allInfo = 1;
+
 
 
 
@@ -21,30 +21,31 @@ int main()
 
 {
 	//asking for information to input
-	allInfo++;
-	cout << "Please give me the information of five family members,\n" 
-		"I will give you the information back.";
-	
-		cout << "\nPlease enter the name of five family members.\n ";
-		for (int i = 0; i <= 4; i++)
-			getline(cin,name[0,1,2,3,4]);
-	
 
-			cout << "\nPlease enter the birth city of five family members. \n";
-			for (int i = 0; i <= 4; i++)
-			getline(cin, birthCity[0,1,2,3,4]);
-			
-			cout << "\nPlease enter the birth year of five family members between 1900-2018.\n ";
-			for (int i = 0; i <= 4; i++)
-				cin >> birthYr[0,1,2,3,4];
+	cout << "Please give me the information of five family members,\n"
+		"I will give you the information back.";
+
+	cout << "\nPlease enter the name of five family members.\n ";
+	for (int i = 0; i <= 4; i++)
+		getline(cin, name[i]);
+
+
+	cout << "\nPlease enter the birth city of five family members. \n";
+	for (int i = 0; i <= 4; i++)
+		getline(cin, birthCity[i]);
+
+	cout << "\nPlease enter the birth year of five family members between 1900-2018.\n ";
+	for (int i = 0; i <= 4; i++)
+		cin >> birthYr[i];
+
 
 //feeding information back out to the user
 
-				for (int i = 0; i <= allInfo; i++)
+				for (int i = 0; i < CAP; i++)
 				{
-					cout << "Name: " << name[0,1,2,3,4] << endl;
-					cout << "Birth City: " << birthCity[0,1,2,3,4] << endl;
-					cout << "Birth Year: " << birthYr[0,1,2,3,4] << endl;
+					cout << "Name: " << name[i] << endl;
+					cout << "Birth City: " << birthCity[i] << endl;
+					cout << "Birth Year: " << birthYr[i] << endl;
                  }
 
 
